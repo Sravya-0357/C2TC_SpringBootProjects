06ae72cf85c172b712f9d423d7e1cb1122786882
DELL <DELL@SravyaPC2>
2025-08-16 11:18:12 +0530

    Spring Boot

---
+++ CourseManagementModule/src/test/java/com/tnsif/courseManagement/CourseManagementModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tnsif.courseManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseManagementModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
